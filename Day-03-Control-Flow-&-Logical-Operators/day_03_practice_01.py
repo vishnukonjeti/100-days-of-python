a6d141e2fab1c99b18664c5e1fafb3073bc8f62e
Vishnu Konjeti <vishnu.konjeti@gmail.com>
2026-02-23 16:15:58 +0530

    Day 03 - Practice 1 completed

--- Day-03-Control-Flow-&-Logical-Operators/day_03_practice_01.py
+++ Day-03-Control-Flow-&-Logical-Operators/day_03_practice_01.py
@@ -1,27 +1,19 @@
-print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
-bill = 0
+"""
+Day 03 - BMI Categorization
+Course: 100 Days of Code - Python Bootcamp (Angela Yu)
 
-if size == "S":
-    bill = 15
-    if pepperoni == "Y":
-        bill += 2
-    if extra_cheese == "Y":
-        bill += 1
-elif size == "M":
-    bill = 20
-    if pepperoni == "Y":
-        bill += 3
-    if extra_cheese == "Y":
-        bill += 1
-elif size == "L":
-    bill = 25
-    if pepperoni == "Y":
-        bill += 3
-    if extra_cheese == "Y":
-        bill += 1
+This program calculates BMI and categorizes it
+as underweight, normal weight, or overweight.
+"""
 
-print(f"Your final bill is: ${bill}.")
+weight = 85
+height = 1.85
 
+bmi = weight / (height ** 2)
+
+if bmi < 18.5:
+    print("underweight")
+elif bmi < 25:
+    print("normal weight")
+else:
+    print("overweight")
